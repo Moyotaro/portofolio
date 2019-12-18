5feb10327ff059b5babcf579f801c88210a7d142
舟見朋夜 <tuttelestradeportanoarome@gmail.com>
2019-12-18 20:45:34 +0900

    終わらん(´-ω-`)

--- assets/js/app5.js
+++ assets/js/app5.js
@@ -1,6 +1,43 @@
 /* Modernizr 2.0.6 (Custom Build) | MIT & BSD
  * Build: http://www.modernizr.com/download/#-iepp-cssclasses-load
  */
+
+
+$(function () {
+
+    var $container = $('#ib-container'),
+        $articles = $container.children('article'),
+        timeout;
+
+    $articles.on('mouseenter', function (event) {
+
+        var $article = $(this);
+        clearTimeout(timeout);
+        timeout = setTimeout(function () {
+
+            if ($article.hasClass('active')) return false;
+
+            $articles.not($article.removeClass('blur').addClass('active'))
+                .removeClass('active')
+                .addClass('blur');
+
+        }, 65);
+
+    });
+
+    $container.on('mouseleave', function (event) {
+
+        clearTimeout(timeout);
+        $articles.removeClass('active blur');
+
+    });
+
+});
+
+
+
+
+
 ; window.Modernizr = function (a, b, c) {
     function x(a, b) {
         return !!~("" + a).indexOf(b)
